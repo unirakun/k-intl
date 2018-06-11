a27b026481f461914a6f38a0125b313d7d460ea5
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-06-11 14:53:01 +0200

    :arrow_up: rollup / eslint-plugin-babel / fix test lang (#22)

--- misc/rollup.config.js
+++ misc/rollup.config.js
@@ -2,7 +2,7 @@ import path from 'path'
 import fs from 'fs'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
 

85da9b68d73681ba1e97d8f60a54b65cdd6935e0
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2019-02-13 10:06:56 +0100

    :ok_hand: test

--- tests/resources/fr-FR.js
+++ tests/resources/fr-FR.js
@@ -4,16 +4,4 @@ export default {
     withOneParam: 'avec {one} paramètre',
     withTwoParam: 'avec {one} {two} paramètre',
   },
-  plural: {
-    simple: 'vous avez {param, plural, =0 {aucun paramètre.} =1 {one paramètre.} other {# paramètres.}}',
-  },
-  number: {
-    withoutParam: 'aucun paramètre',
-    withOneParam: 'avec {one} paramètre',
-    withPercentParam: 'avec {one} paramètre',
-    withUSDFormat: 'avec {one} paramètre',
-  },
-  select: {
-    gender: '{ gender, select, male {Il} female {Elle} other {ça} } répondra sous peu.',
-  },
 }
